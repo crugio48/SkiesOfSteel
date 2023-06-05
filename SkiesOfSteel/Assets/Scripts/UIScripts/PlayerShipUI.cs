35249996aa09f8373315cf4f3d9fdc7cb2bc047a
AndreaZaniboni1 <zanna1000@gmail.com>
2023-06-05 17:40:41 +0200

    FirstCanvas

---
+++ SkiesOfSteel/Assets/Scripts/UIScripts/PlayerShipUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipUI : MonoBehaviour
+{
+    ShipUnit ship;
+
+    void ButtonGeneration()
+    {
+        ship.
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
