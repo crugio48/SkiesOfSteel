718c258df61fcf07491eae1029e17ccfbe2bbe9c
AndreaZaniboni1 <zanna1000@gmail.com>
2023-06-12 17:53:17 +0200

    Added Stuff UI

---
+++ SkiesOfSteel/Assets/Scripts/UIScripts/TurnCanvas.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnCanvas : MonoBehaviour
+{
+    List<string> PlayerNames;
+
+    void Update()
+    {
+        //Take the names of the players and show them into 
+        //transform.GetChild(0).GetChild(1).GetComponentInChildren<Text>().text = "Current Turn = " + PlayerNames[1];
+        //transform.GetChild(0).GetChild(1).GetComponentInChildren<Text>().text = "Current Turn = " + PlayerNames[1+1];
+
+    }
+}
